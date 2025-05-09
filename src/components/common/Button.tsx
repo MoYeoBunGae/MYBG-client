db7d539975c999f0a16f824366eb1709ee45fae1
yeonuy <jyeonuy@gmail.com>
2025-05-09 20:08:35 +0900

    ✨ feat: 버튼 컴포넌트 업데이트 - 스타일 추가 (#6)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,8 +1,9 @@
 import RightArrowIcon from '@/assets/icons/right-arrow.svg?react';
+import DownArrowIcon from '@/assets/icons/down-arrow.svg?react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
-  variant?: 'normal' | 'round' | 'withIcon' | 'onlyIcon';
+  variant?: 'normal' | 'round' | 'roundThin' | 'withIcon' | 'onlyIcon';
   color?:
     | 'primary'
     | 'white'
@@ -31,6 +32,7 @@ const Button = ({
       className={`inline-flex items-center justify-center focus:outline-none min-w-max cursor-pointer
         ${variant === 'normal' ? 'w-full h-10 rounded-md font-semibold' : 'text-xs font-medium'}
         ${variant === 'round' ? 'w-fit h-fit px-3 py-2 rounded-full' : ''}
+        ${variant === 'roundThin' ? 'w-fit px-2.5 py-1 rounded-full' : ''}
         ${variant === 'withIcon' ? 'w-full h-fit px-4 py-2 rounded-md' : ''}
         ${variant === 'onlyIcon' ? 'size-12 rounded-full' : ''}
         ${color === 'primary' ? 'bg-primary text-white' : ''}
@@ -57,6 +59,11 @@ const Button = ({
       {variant === 'withIcon' && (
         <span>
           <RightArrowIcon className={`size-3 ml-2 ${color === 'white' ? 'text-black60' : ' '}`} />
+        </span>
+      )}
+      {variant === 'roundThin' && (
+        <span>
+          <DownArrowIcon className={`size-3.5 mt-0.5 ml-1 text-black84`} />
         </span>
       )}
     </button>
